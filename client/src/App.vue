e92c7d7a6fc58c1ce31c58b13dfda05c34bec922
agungsyon <ahmatagung645@gmail.com>
2023-10-25 05:18:07 +0700

    feat: skeleton vuejs

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="vh100 overflow-hidden">
+    <CustomNavbar/>
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import CustomNavbar from './components/CustomNavbar.vue';
+export default {
+  name: "App",
+  components: {
+    CustomNavbar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1>homepage</h1>
+  </main>
+</template>
+
+<script setup></script>
